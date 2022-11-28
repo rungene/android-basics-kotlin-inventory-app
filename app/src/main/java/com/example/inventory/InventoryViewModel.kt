a4d6a150c665e24a126b5bf663d964e69d42c7a0
rungene <rungenesamuel@gmail.com>
2022-11-28 21:44:51 +0300

    refactor:Implement the create() method

--- app/src/main/java/com/example/inventory/InventoryViewModel.kt
+++ app/src/main/java/com/example/inventory/InventoryViewModel.kt
@@ -8,8 +8,12 @@ class InventoryViewModel(private val itemDao: ItemDao) : ViewModel() {
 
 }
 class InventoryViewModelFactory(private val itemDao: ItemDao) : ViewModelProvider.Factory {
-    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-        TODO("Not yet implemented")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(InventoryViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return InventoryViewModel(itemDao) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
     }
 
 }
