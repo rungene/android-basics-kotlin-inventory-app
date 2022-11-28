a00b91fe154130917d3504a4ecf28e4fb28e0d4e
rungene <rungenesamuel@gmail.com>
2022-11-28 05:13:06 +0300

    refactor:declare an abstract function that returns the ItemDao

--- app/src/main/java/com/example/inventory/data/ItemRoomDatabase.kt
+++ app/src/main/java/com/example/inventory/data/ItemRoomDatabase.kt
@@ -4,4 +4,8 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 
 @Database(entities = [Item::class], version = 1, exportSchema = false)
-abstract class ItemRoomDatabase : RoomDatabase() {}
+abstract class ItemRoomDatabase : RoomDatabase() {
+
+    abstract fun itemDao(): ItemDao
+
+}
