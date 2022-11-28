b094d52cd00c193806e3c95cf001f9466f533551
rungene <rungenesamuel@gmail.com>
2022-11-28 05:53:33 +0300

    refactor: Create an Inventory viewmodel

--- app/src/main/java/com/example/inventory/InventoryViewModel.kt
+++ app/src/main/java/com/example/inventory/InventoryViewModel.kt
@@ -1,2 +1,8 @@
 package com.example.inventory
 
+import androidx.lifecycle.ViewModel
+import com.example.inventory.data.ItemDao
+
+class InventoryViewModel(private val itemDao: ItemDao) : ViewModel() {
+    
+}
