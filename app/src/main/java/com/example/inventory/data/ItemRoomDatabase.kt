8ba8c257647b62d6b8733eb0363677a2d9c44b6b
rungene <rungenesamuel@gmail.com>
2022-11-28 05:30:01 +0300

    refactor: create a val instance variable, and use the database builder to get the database.

--- app/src/main/java/com/example/inventory/data/ItemRoomDatabase.kt
+++ app/src/main/java/com/example/inventory/data/ItemRoomDatabase.kt
@@ -2,6 +2,7 @@ package com.example.inventory.data
 
 import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(entities = [Item::class], version = 1, exportSchema = false)
@@ -14,7 +15,10 @@ abstract class ItemRoomDatabase : RoomDatabase() {
         private var INSTANCE: ItemRoomDatabase? = null
 
         fun getDatabase(context: Context): ItemRoomDatabase {
-            return INSTANCE ?: synchronized(this) { }
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder()
+
+            }
 
         }
 
