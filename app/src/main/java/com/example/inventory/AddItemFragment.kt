e10e9979707ddd5376a9135ad9d03d83929afaac
rungene <rungenesamuel@gmail.com>
2022-11-29 05:04:10 +0300

    refactor:Add another private function called addNewItem()

--- app/src/main/java/com/example/inventory/AddItemFragment.kt
+++ app/src/main/java/com/example/inventory/AddItemFragment.kt
@@ -60,7 +60,18 @@ class AddItemFragment : Fragment() {
     }
 
     private fun isEntryValid(): Boolean {
+        return viewModel.isEntryValid(
+            binding.itemName.text.toString(),
+            binding.itemPrice.text.toString(),
+            binding.itemCount.text.toString()
+        )
     }
+
+    private fun addNewItem() {
+        if (isEntryValid()) {
+        }
+    }
+
 
     /**
      * Called before fragment is destroyed.
