f0aa8a4f70b8203105a7f1f9f01343d637d45f3e
rungene <rungenesamuel@gmail.com>
2022-11-29 09:29:58 +0300

    refactor:Retrieve item details

--- app/src/main/java/com/example/inventory/InventoryViewModel.kt
+++ app/src/main/java/com/example/inventory/InventoryViewModel.kt
@@ -14,6 +14,13 @@ class InventoryViewModel(private val itemDao: ItemDao) : ViewModel() {
         }
     }
 
+    /**
+     * Retrieve an item from the repository.
+     */
+    fun retrieveItem(id: Int): LiveData<Item> {
+        return itemDao.getItem(id).asLiveData()
+    }
+
     private fun getNewItemEntry(itemName: String, itemPrice: String, itemCount: String): Item {
         return Item(
             itemName = itemName,
